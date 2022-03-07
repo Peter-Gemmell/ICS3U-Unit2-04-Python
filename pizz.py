7eef7a867f4598430daa75476ef37f8e9033f84e
Peter Gemmell <peter.gemmell@mths.ca>
2022-03-07 19:08:07 +0000

    Palindrome

--- pizz.py
+++ pizz.py
@@ -13,15 +13,15 @@ def main():
     # input
     diameter = int(input("Enter the diameter of the pizza you would like (inch): "))
 
-    #process
-    sub_total = (constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH))
+    # process
+    sub_total = constants.LABOR + constants.RENT + (diameter * constants.COST_PER_INCH)
     total = sub_total + (sub_total * constants.HST)
 
-    #output
+    # output
     print("")
     print("The cost of a {0} inch pizza is ${1:,.2f}".format(diameter, total))
-    print("\nFinished.")
-    
+    print("\nDone.")
+
+
 if __name__ == "__main__":
     main()
-    
